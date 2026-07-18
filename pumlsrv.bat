4c04914b09aa3db1c9994df168137eef052d3367
Michael Schulte <michael.schulte@smartclip.tv>
2026-07-18 10:46:37 +0200

    bumped version to 2.1.1

--- pumlsrv.bat
+++ pumlsrv.bat
@@ -1,1 +1,1 @@
-java -jar target\pumlsrv-2.1.0-jar-with-dependencies.jar %*
+java -jar target\pumlsrv-2.1.1-jar-with-dependencies.jar %*
--- pumlsrv.sh
+++ pumlsrv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar target/pumlsrv-2.1.0-jar-with-dependencies.jar $@
+java -jar target/pumlsrv-2.1.1-jar-with-dependencies.jar $@
 
